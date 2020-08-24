a112aca2c9985c50a144fb732f1e1ca962d64ad2
masahito-nagayama <me18sf67m@gmail.com>
2020-08-24 21:27:30 +0900

    aaaa

--- db/migrate/20200807182555_create_boards.rb
+++ db/migrate/20200807182555_create_boards.rb
@@ -3,7 +3,7 @@ class CreateBoards < ActiveRecord::Migration[5.0]
     create_table :boards do |t|
       t.string :name
       t.string :title
-      t.text :bodys
+      t.text :body
 
       t.timestamps
     end
